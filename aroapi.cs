4d3c647da6cff9b6c3979789cced5aacfb914bc7
Christian Morris <116536697+DevChristianMorris@users.noreply.github.com>
2024-06-06 08:52:49 +1200

    Added AroAPI and set up files / readme

---
+++ aroapi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+public class AroFloService
+{
+    private readonly string _baseUrl = "https://api.aroflo.com";
+    private readonly string _apiKey = "your_api_key_here"; // Add API key
+
+    public JArray FetchData()
+    {
+        var client = new RestClient(_baseUrl);
+        var request = new RestRequest("path/to/your/endpoint", Method.GET); // Add Endpointpath
+        request.AddHeader("Authorization", $"Bearer {_apiKey}");
+
+        var response = client.Execute(request);
+
+        if (!response.IsSuccessful)
+            throw new Exception("Failed to fetch data from AroFlo");
+
+        return JArray.Parse(response.Content);
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+NuGet packages
+
+dotnet add package System.DirectoryServices.Protocols
+dotnet add package Newtonsoft.Json
+dotnet add package RestSharp
